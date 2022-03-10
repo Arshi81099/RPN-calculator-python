8d73480dcc6c7951cccb61b6e3e08f9e590177ec
Arshi Khan <arshikhan81099@gmail.com>
2022-03-10 19:37:58 +0530

    first commited

---
+++ cal.py
@@ -0,0 +1,42 @@
+
+import operator
+
+ops = { '+': operator.add,
+        '-': operator.sub,
+        '*': operator.mul,
+        '/': operator.floordiv
+}
+
+def eval_expression(tokens, stack):
+  
+    for token in tokens:
+        if set(token).issubset(set("0123456789.")):
+            stack.append(float(token))
+        elif token in ops:
+            if len(stack) < 2:
+                raise ValueError('Must have at least two parameters to perform op')
+            a = stack.pop()
+            b = stack.pop()
+            op = ops[token]
+            if(op == operator.floordiv and a == 0):
+              print("Division not possible")
+              raise ZeroDivisionError("Zero cannot divide a number")
+           
+            stack.append(op(b,a))
+        else:
+            raise ValueError("What is this? %s" % token)
+    return stack
+
+if __name__ == '__main__':
+    stack = []
+    while True:
+        expression = input('> ')
+        if expression in ['quit','q','exit']:
+            exit()
+        elif expression in ['clear','empty']:
+            stack = []
+            continue
+        elif len(expression)==0:
+            continue
+        stack = eval_expression(expression.split(), stack)
+        print(stack)
